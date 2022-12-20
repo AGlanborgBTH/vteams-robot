0261716600b27858bdd441c61a7320c8043c204e
TinkzBot <zakaria.ziadi92@gmail.com>
2022-12-20 11:41:36 +0100

    Rebuilt Robot To Run Faster.

--- robot.py
+++ robot.py
@@ -3,33 +3,46 @@ import requests
 import random
 import time
 
-# Update the lat and long for an object's destination with a given id
+@profile
 def update_destination(object_id):
-  # Generate a random latitude and longitude
-  lat = random.uniform(-90, 90)
-  long = random.uniform(-180, 180)
 
-  # Print the object id and the new lat and long values
   print(f'Updating destination for object: {object_id}')
   print(f'  New Lat = {lat}')
-  print(f'  New Long = {long}')
+  print(f'  New Long = {lng}')
 
-  # Use a PATCH request to update the lat and long for the object's destination with the given id
-  requests.patch(f'http://localhost:3000/v1/scooters/{object_id}', json={'destination': {'lat': lat, 'long': long}})
+  requests.patch(f'http://localhost:3000/v1/scooters/{object_id}', json={'destination': {'lat': lat, 'lng': lng}})
 
-# Loop through all objects in the API
+
 while True:
-  # Use a GET request to retrieve the list of objects
+  start_time = time.perf_counter()
   response = requests.get('http://localhost:3000/v1/scooters')
   objects = response.json()
 
   # Loop through all objects
   for object in objects['data']:
+    lat = None
+    lng = None
+
+    if object['city'] == str('Göteborg'):
+      lat = random.uniform(57.687300367053496, 57.71646139531549)
+      lng = random.uniform(11.888231715434216, 12.013669635629554)
+      # print("Göteborg")
+
+    elif object['city'] == str('Linköping'):
+      lat = random.uniform(58.406333, 58.422808)
+      lng = random.uniform(15.592416, 15.648206)
+      # print("Linköping")
+
+    elif object['city'] == str('Uppsala'):
+      lat = random.uniform(17.603621, 17.698512)
+      lng = random.uniform(59.8439, 59.874093)
+      # print("Uppsala")
+
     # Check if the object is in use
     if object['inUse']:
       # Update the destination for the object
       thread = threading.Thread(target=update_destination, args=(object['_id'],))
       thread.start()
-
-  # Sleep for 1 second before checking the objects again
-  time.sleep(1)
+      end_time = time.perf_counter()
+      elapsed_time = end_time - start_time
+      print("The function took %f seconds to execute" % elapsed_time)
